f1852cdde97f9e3c54b19132418b8ddc141dbd81
Jocelyn Naarden <tzionone@gmail.com>
2022-06-01 07:12:44 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import Parent from "./components/Parent"
+// import Form from './components/Form'
 
 ReactDOM.render(
-  <Form />,
+  <Parent />,
   document.getElementById('root')
 );
